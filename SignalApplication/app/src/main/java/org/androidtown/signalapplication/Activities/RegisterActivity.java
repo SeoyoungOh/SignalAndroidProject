e883d4e8962d537f5282c0da499923adf4bd0c4d
SeoyoungOh <dkdlel9306@gmail.com>
2017-11-19 22:39:36 +0900

    Register Activity

--- SignalApplication/app/src/main/java/org/androidtown/signalapplication/Activities/RegisterActivity.java
+++ SignalApplication/app/src/main/java/org/androidtown/signalapplication/Activities/RegisterActivity.java
@@ -2,6 +2,11 @@ package org.androidtown.signalapplication.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import org.androidtown.signalapplication.R;
 
@@ -11,5 +16,23 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText mUserID = (EditText) findViewById(R.id.user_id);
+        EditText mUserPW = (EditText) findViewById(R.id.user_pw);
+        EditText mUserPWCon = (EditText) findViewById(R.id.user_pw_con);
+        EditText mUserName = (EditText) findViewById(R.id.user_name);
+        EditText mUserJob = (EditText) findViewById(R.id.user_occu);
+        EditText mUserPhone = (EditText) findViewById(R.id.user_phone);
+
+        // img click -> image crop
+        ImageView mUserProfileImg = (ImageView) findViewById(R.id.add_profile_img);
+
+        // keyboard
+        LinearLayout mRegistLayout = (LinearLayout) findViewById(R.id.register_layout);
+
+        // send server & get response
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+
+
     }
 }
