847249125ae21ee4116ab2d839d12e680a9c7ee3
SeoyoungOh <dkdlel9306@gmail.com>
2017-11-19 20:20:49 +0900

    Intro again

---
+++ SignalApplication/app/src/main/java/org/androidtown/signalapplication/Activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package org.androidtown.signalapplication.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidtown.signalapplication.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
