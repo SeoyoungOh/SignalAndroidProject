68f3a0c05e28bba18eb43634dc09d892a1e791be
SeoyoungOh <dkdlel9306@gmail.com>
2017-11-19 23:29:46 +0900

    Register Activity

--- SignalApplication/app/src/main/java/org/androidtown/signalapplication/Activities/RegisterActivity.java
+++ SignalApplication/app/src/main/java/org/androidtown/signalapplication/Activities/RegisterActivity.java
@@ -58,7 +58,7 @@ public class RegisterActivity extends AppCompatActivity {
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
